4b3f827fc3a5dcfa59f6b0f28186efa3b7aab403
RanveerRathour <ranveerrathour08@gmail.com>
2023-03-05 21:30:58 +0530

    first code

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:music/screen1.dart';
+
+void main(){
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: call1(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
